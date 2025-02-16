5dc9cf50f4d1d1fb330c3a0c311eeaede091b247
Joseph Muller <jtmuller5@gmail.com>
2025-02-16 15:09:33 -0500

    Move global.d.ts to renderer

--- src/client/src/features/home/HomeView.tsx
+++ src/client/src/features/home/HomeView.tsx
@@ -1,3 +1,17 @@
 export default function HomeView() {
-  return <div>HomeView</div>;
+  return (
+    <div>
+      <div>Node: {window.versions.node()}</div>
+      <div>Chrome: {window.versions.chrome()}</div>
+      <div>Electron: {window.versions.electron()}</div>
+      <button
+        onClick={async () => {
+          const response = await window.versions.ping();
+          alert(response); // Display the response from ping()
+        }}
+      >
+        Click Me
+      </button>
+    </div>
+  );
 }
